667f9d9512d71211ae370a0df7b1c7bbba03199c
Angie <angieari97@hotmail.com>
2022-08-31 19:00:59 -0500

    Clase MovimientoDinero

--- src/main/java/com/UdeACiclo3/BikerHouse/modelos/MovimientoDinero.java
+++ src/main/java/com/UdeACiclo3/BikerHouse/modelos/MovimientoDinero.java
@@ -1,5 +1,87 @@
 package com.UdeACiclo3.BikerHouse.modelos;
 
-public class MovimientoDinero {
+import javax.persistence.*;
 
+@Entity
+@Table(name="Movimientos")
+public class MovimientoDinero {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int id;
+    private long monto;
+    private String concepto;
+    @ManyToOne
+    @JoinColumn(name= "Movimientos_id" )
+    private Empleado usuario;
+
+    //METODOS:
+
+    // Metodo leerMonto
+
+    public long leerMonto() {
+        return getMonto();
+    }
+    //Metodo modificar Monto
+    public void ModificarMonto(long Newmonto){
+        setMonto(Newmonto);
+    }
+    //Metodo leer Concepto de Dinero
+    public String leerConcepto(){
+        return getConcepto();
+    }
+    //Metodo Modificar Concepto de Dinero
+    public void ModificarConcepto(String newConcepto){
+        setConcepto(newConcepto);
+    }
+
+    //Metodo para mostra el usuario encargado de registrar el movimiento
+    public String LeerEmpleadoMovimiento(){
+        return getUsuario().getNombre();
+    }
+
+
+
+
+//CONSTRUCTOR
+
+    public MovimientoDinero(long monto, String concepto, Empleado usuario) {
+        this.monto = monto;
+        this.concepto = concepto;
+        this.usuario = usuario;
+    }
+
+    //GET Y SET PARA LEER Y MODIFICAR LOS ATRIBUTOS
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public long getMonto() {
+        return monto;
+    }
+
+    public void setMonto(long monto) {
+        this.monto = monto;
+    }
+
+    public String getConcepto() {
+        return concepto;
+    }
+
+    public void setConcepto(String concepto) {
+        this.concepto = concepto;
+    }
+
+    public Empleado getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Empleado usuario) {
+        this.usuario = usuario;
+    }
 }
+
+
