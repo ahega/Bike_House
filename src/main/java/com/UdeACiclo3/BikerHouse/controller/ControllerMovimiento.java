20039ebf273259a4dad864334887226673defe2d
Anngiw <angieari@97hotmail.com>
2022-09-17 01:33:49 -0500

    ControllerMovimientos +Templates(Editar y Eliminar)

--- src/main/java/com/UdeACiclo3/BikerHouse/controller/ControllerMovimiento.java
+++ src/main/java/com/UdeACiclo3/BikerHouse/controller/ControllerMovimiento.java
@@ -2,11 +2,10 @@ package com.UdeACiclo3.BikerHouse.controller;
 import com.UdeACiclo3.BikerHouse.service.UsuarioService;
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.UdeACiclo3.BikerHouse.modelos.MovimientoDinero;
 import com.UdeACiclo3.BikerHouse.modelos.Usuario;
@@ -23,9 +22,9 @@ public class ControllerMovimiento {
     @GetMapping("/VerMovimientos")
     public String viewMovimientos(Model model, @ModelAttribute("mensaje") String mensaje){
         List<MovimientoDinero> listaMovimientos=movimientoDineroService.traerTodoMovimientoDinero();
-        model.addAttribute("movimientoDineroList",listaMovimientos);
+        model.addAttribute("movlist",listaMovimientos);
         model.addAttribute("mensaje",mensaje);
-        return "verMovimientos"; //Llamamos al HTML
+        return "verMovimientos";
     }
 
     @GetMapping("/AgregarMovimiento")
@@ -48,6 +47,37 @@ public class ControllerMovimiento {
         redirectAttributes.addFlashAttribute("mensaje","saveError");
         return "redirect:/AgregarMovimiento";
     }
+
+    @GetMapping("/EditarMovimiento/{id}")
+    public String editarMovimiento(Model model, @PathVariable Integer id, @ModelAttribute("mensaje") String mensaje){
+        MovimientoDinero mov= movimientoDineroService.traerMovimientoDineroPorId(id);
+        model.addAttribute("mov", mov);
+        List<Usuario>listaUsuarios= usuarioService.getAllUsuarios();
+        model.addAttribute("usuariolist",listaUsuarios);
+        return "editarMovimiento";
+    }
+
+    @PostMapping("/ActualizarMovimiento")
+    public String actualizarMovimiento(@ModelAttribute("mov") MovimientoDinero mov, RedirectAttributes redirectAttributes){
+        if(movimientoDineroService.crearOActualizarMovimientoDinero(mov)){
+            redirectAttributes.addFlashAttribute("mensaje", "updateOK");
+            return "redirect:/VerMovimientos";
+        }
+        redirectAttributes.addFlashAttribute("mensaje", "updateError");
+        return "redirect:/EditarMovimientos/"+mov.getId();
+    }
+
+    @GetMapping("/EliminarMovimiento/{id}")
+    public String eliminarMovimiento(@PathVariable Integer id, RedirectAttributes redirectAttributes){
+        if(movimientoDineroService.eliminarMovimientoDinero(id)){
+            redirectAttributes.addFlashAttribute("mensaje","saveOK");
+            return "redirect:/VerMovimientos";
+        }
+        redirectAttributes.addFlashAttribute("mensaje","saveError");
+        return "redirect:/VerMovimientos";
+
+    }
+
 }
 
 
